e3819ac7073bc83340602fa498344e1df4258fa3
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-02-20 11:40:31 -0500

    nothing changed

--- app/src/main/java/hackmcst/speach/LevelOneActivity.java
+++ app/src/main/java/hackmcst/speach/LevelOneActivity.java
@@ -9,6 +9,5 @@ public class LevelOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
-        
     }
 }
