600e242733c7f8846b0d97b299a4251827ce3276
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-02-19 23:38:49 -0500

    menu buttons point to level activities

--- app/src/main/java/hackmcst/speach/MenuActivity.java
+++ app/src/main/java/hackmcst/speach/MenuActivity.java
@@ -1,7 +1,9 @@
 package hackmcst.speach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void goToLevelOne(View v)
+    {
+        Intent i = new Intent(this, LevelOneActivity.class);
+        startActivity(i);
+    }
+
+    public void goToLevelTwo(View v)
+    {
+        Intent i = new Intent(this, LevelTwoActivity.class);
+        startActivity(i);
+    }
+
+    public void goToLevelThree(View v)
+    {
+        Intent i = new Intent(this, LevelThreeActivity.class);
+        startActivity(i);
     }
 }
