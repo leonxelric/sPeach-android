6a3fe8966fb67c0863fbd7dc8e3f1eeb5a893a53
LEON-LAPTOP\leonx <lxie21@mlschools.org>
2018-02-22 20:32:23 -0500

    small changes, attempt to make UI

--- app/src/main/java/hackmcst/speach/LevelOneActivity.java
+++ app/src/main/java/hackmcst/speach/LevelOneActivity.java
@@ -1,7 +1,9 @@
 package hackmcst.speach;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LevelOneActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LevelOneActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_one);
+    }
+
+    public void goToMenu(View v)
+    {
+        Intent i = new Intent(this, MenuActivity.class);
+        startActivity(i);
     }
 }
